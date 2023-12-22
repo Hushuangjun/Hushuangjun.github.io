4314f7db262b8ef9f0a3e5f177cc0eb7801ad7a0
Hushuangjun <husj0711@outlook.com>
2023-12-22 23:49:04 +0800

    Site updated: 2023-12-22 23:49:03

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -5,16 +5,20 @@ document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         changePageState('╭(°A°`)╮ 页面崩溃啦 ~');
     } else {
-        changePageState('(ฅ>ω<*ฅ) 噫又好了~' + originalTitle);
+        // Do nothing when the page becomes visible
     }
+});
+
+// Listen for the focus event
+window.addEventListener('focus', function () {
+    changePageState('(ฅ>ω<*ฅ) 噫又好了~' + originalTitle);
+
+    // Delay for 3 seconds and then restore the original title
+    setTimeout(function () {
+        document.title = originalTitle;
+    }, 3000);
 });
 
 function changePageState(newTitle) {
     document.title = newTitle;
-    clearTimeout(titleTime);
-
-    // Restore the original title after 2 seconds
-    titleTime = setTimeout(function () {
-        document.title = originalTitle;
-    }, 2000);
 }
