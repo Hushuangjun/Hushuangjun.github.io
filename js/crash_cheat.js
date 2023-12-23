9b38341486b78805af5b232b0591856ab3e13cfc
Hushuangjun <husj0711@outlook.com>
2023-12-24 01:31:48 +0800

    Site updated: 2023-12-24 01:31:46

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -5,20 +5,29 @@ document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         changePageState('╭(°A°`)╮ 页面崩溃啦 ~');
     } else {
-        // Do nothing when the page becomes visible
+        // Check if the title was set to '(ฅ>ω<*ฅ) 噫又好了~'
+        // If yes, restore the original title immediately
+        if (document.title === '(ฅ>ω<*ฅ) 噫又好了~') {
+            document.title = originalTitle;
+        }
     }
 });
 
 // Listen for the focus event
 window.addEventListener('focus', function () {
-    changePageState('(ฅ>ω<*ฅ) 噫又好了~');
+    // Check if the title was set to '╭(°A°`)╮ 页面崩溃啦 ~'
+    // If yes, restore the original title after 3 seconds
+    if (document.title === '╭(°A°`)╮ 页面崩溃啦 ~') {
+        changePageState('(ฅ>ω<*ฅ) 噫又好了~');
 
-    // Delay for 3 seconds and then restore the original title
-    setTimeout(function () {
-        document.title = originalTitle;
-    }, 3000);
+        // Delay for 3 seconds and then restore the original title
+        titleTime = setTimeout(function () {
+            document.title = originalTitle;
+        }, 3000);
+    }
 });
 
 function changePageState(newTitle) {
     document.title = newTitle;
+    clearTimeout(titleTime); // Clear the previous timeout to ensure proper timing
 }
